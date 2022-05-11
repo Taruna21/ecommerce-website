059c4cfe77c609dbdc938fc9a7c3762fbf0306c4
Taruna <pateltaruna552@gmail.com>
2022-05-11 09:24:36 +0530

    Uploading file with some errors to resolve

--- store/views.py
+++ store/views.py
@@ -18,21 +18,40 @@ def store(request):
 def cart(request):
     
     if request.user.is_authenticated:
-        customer = request.user.custmer
+        customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitems_set.all()
         
+        
     else:
         items=[]
+        order={'get_cart_total':0,  'get_cart_items' :0 }
+        
     
-    context = {'items' : items}
+    context = {'items' : items, 'order' : order}
     return render(request, 'store/cart.html', context)
 
 
 
 
 
+
+
 def checkout(request):
-	context={}
-	return render(request, 'store/checkout.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitems_set.all()
+        
+        
+    else:
+        items=[]
+        order={'get_cart_total':0,  'get_cart_items' :0 }
+        
+       
+       
+       
+    context = {'items' : items, 'order' : order}
+    return render(request, 'store/checkout.html', context)
+	
 
